915e7149343cb20e72d59380a2f6a58801c8cf27
youngtitanium <youngtitanium.l@gmail.com>
2020-11-10 10:04:12 +0200

    Creation the tree completed

--- tree.py
+++ tree.py
@@ -1,4 +1,9 @@
-from telegram.ext import Updater, InlineQueryHandler, CommandHandler
+from telegram.ext import (
+    Updater, InlineQueryHandler,
+    CommandHandler, ConversationHandler,
+    MessageHandler, Filters,
+    PicklePersistence
+    )
 import requests
 import re
 import html
@@ -8,7 +13,10 @@ import json
 import logging
 import traceback
 from telegram.ext import CallbackQueryHandler
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup , ReplyKeyboardMarkup , KeyboardButton
+from telegram import (
+    InlineKeyboardButton, InlineKeyboardMarkup,
+    ReplyKeyboardMarkup , KeyboardButton,
+    )
 import csv
 
 
@@ -44,22 +52,18 @@ def menu_actions(update,context):
     bot = context.bot
     query = update.callback_query
     
-    if query.data == 'm1':
-
+##    if query.data == 'm1':
+        #Registered the new handler for this!
         # update.callback_query.message.reply_text("Enter the Earliest Ancestor:")
         
-        update.effective_message("Enter the Earliest Ancestor:")
-        with open('Tree.csv', 'w', newline='') as file:
-            writer = csv.writer(file)
-            text_1 = update.effective_message.text
-            writer.writerow([text_1]) 
+##        update.effective_message("Enter the Earliest Ancestor:")
+##        with open('Tree.csv', 'w', newline='') as file:
+##            writer = csv.writer(file)
+##            text_1 = update.effective_message.text
+##            writer.writerow([text_1]) 
                    
-        
 
-
-
-
-    elif query.data == 'm2':
+    if query.data == 'm2':
         # second submenu
         # first submenu
         menu_2 = [[InlineKeyboardButton('Submenu 2-1', callback_data='m2_1')],
@@ -186,36 +190,453 @@ def error_handler(update: Update, context: CallbackContext) -> None:
     )
 
     # Finally, send the message
-    context.bot.send_message(chat_id=DEVELOPER_CHAT_ID, text=message, parse_mode=ParseMode.HTML)
+    #context.bot.send_message(chat_id=DEVELOPER_CHAT_ID, text=message, parse_mode=ParseMode.HTML)
+
+
+def manageCallbacks (update, context):
+    query = update.callback_query
+    if query.data in ('m2'):
+        menu_actions(update, context)
+    elif query.data == 'm1_1':
+        first_submenu(update, context)
+    elif query.data == 'm2_1':
+        second_submenu(update, context)
+    else:
+        query.answer("Nothing found!")
+
+
+def getRelations ():
+    kb = InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        text = 'Child',
+                        callback_data = 'addPersonRelation Child'
+                        ),
+                    InlineKeyboardButton(
+                        text = 'Spouse',
+                        callback_data = 'addPersonRelation Spouse'
+                        )
+                    ],
+                [
+                    InlineKeyboardButton(
+                        text = 'Earlier Ancestor',
+                        callback_data = 'addPersonRelation EA'
+                        )
+                    ]
+                ]
+            )
+    return kb
+
+def getGenders ():
+    kb = InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        text = 'male',
+                        callback_data = 'setgender male'
+                        ),
+                    InlineKeyboardButton(
+                        text = 'female',
+                        callback_data = 'setgender female'
+                        )
+                    ]
+                ]
+            )
+    return kb
+
+class ConversationFirstMenu:
+
+    def entry (update, context):
+        context.bot.send_message(
+            chat_id = update.callback_query.message.chat_id,
+            text = 'Okay, enter the Earliest Ancestor.'
+            )
+        return 'getEA'
+
+    def setEarliestAncestor (update, context):
+        
+        Message = update.message
+        EarliestAncestor = Message.text
+        
+        #If user exist get him data else create empty dict
+        if Message.from_user.id in context.bot_data:
+            data = context.bot_data[Message.from_user.id]
+        else:
+            data = {}
+
+        #Update user data
+        data[EarliestAncestor] = {"relations": ["Earliest Ancestor"], "gender": None, "selected": True}
+        context.bot_data[Message.from_user.id] = data
+
+        #Save datafile
+        context.dispatcher.persistence.flush()
+
+        #Get gender keyboard from getGenders
+        keyboard = getGenders()
+        
+        Message.reply_text(
+            "Set the {} gender: ".format(EarliestAncestor),
+            reply_markup = keyboard
+            )
+        return "callbacks"
+
+    def changePersonRelations (update, context):
+        Query = update.callback_query
+
+        #Retrieve userdata
+        data = context.bot_data[Query.from_user.id]
+
+        #Get selected person
+        for personName, personDetails in data.items():
+            if personDetails['selected']:
+                break
+
+        #Update userdata
+        personRelation = Query.data.split()[1]
+        if personRelation == 'EA':
+            for pN, pD in data.items():
+                if "Earliest Ancestor" in pD["relations"]:
+                    Query.answer(
+                        text = "Earliest ancestor must be one!",
+                        show_alert = True
+                        )
+                    return
+        if personRelation == 'EA':
+            personRelation = 'Earliest Ancestor'
+        personDetails['relations'].append({personRelation: None})
+        
+        data[personName] = personDetails
+        context.bot_data[Query.from_user.id] = data
+
+        #Save changes
+        context.dispatcher.persistence.flush()
+
+        options = {
+            "Earliest Ancestor": None,
+            'Child': 'parent',
+            'Spose': 'spouse'
+            }
+
+        Query.message.edit_text(
+            text = "Enter the name {} for {}".format(options[personRelation], personName)
+            )
+
+        return 'setRelation2ndPerson'
+
+    def setRelationFor (update, context):
+        Message = update.message
+
+        secondPersonName = Message.text
+
+        data = context.bot_data[Message.from_user.id]
+
+        for personName, personDetails in data.items():
+            if personDetails['selected']:
+                break
+
+        for index, relation in enumerate(personDetails['relations']):
+            for name, secondPerson in relation.items():
+                if secondPerson is None:
+                    personDetails['relations'][index] = {name: secondPersonName}
+                    break
+
+        data[personName] = personDetails
+        context.bot_data[Message.from_user.id] = data
+
+        context.dispatcher.persistence.flush()
+
+        keyboard = InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        text = 'Yes!',
+                        callback_data = 'haveOtherRelations yes'
+                        ),
+                    InlineKeyboardButton(
+                        text = 'No!',
+                        callback_data = 'haveOtherRelations no'
+                        )
+                    ]
+                ]
+            )
+                
+        Message.reply_text(
+            text = 'Successfully updated relation {0} to {1}! Have {0} other relations?'.format(personName, secondPersonName),
+            reply_markup = keyboard
+            )
+        
+        return 'callbacks'
+        
+        
+
+    def changePersonGender (update, context):
+
+        Query = update.callback_query
+
+
+        data = context.bot_data[Query.from_user.id]
+
+        for personName, personDetails in data.items():
+            if personDetails['selected']:
+                break
+
+        #Update the data
+        selectedGender = Query.data.split()[1]
+        personDetails['gender'] = selectedGender
+        data[personName] = personDetails
+        context.bot_data[Query.from_user.id] = data
+
+        #Save datafile
+        context.dispatcher.persistence.flush()
+
+        if len(data[personName]['relations'])<1:
+            #Retrieve relations keyboard from getRelations
+            keyboard = getRelations()
+
+            #Give the user a choice relations for person
+            Query.message.edit_text(
+                text = 'Select the {} relation:'.format(personName),
+                reply_markup = keyboard
+                )
+            #Change dialog state
+            return 'callbacks'
+
+        keyboard = InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        text = 'Yes!',
+                        callback_data = 'haveOtherRelations yes'
+                        ),
+                    InlineKeyboardButton(
+                        text = 'No!',
+                        callback_data = 'haveOtherRelations no'
+                        )
+                    ]
+                ]
+            )
+        Query.message.edit_text(
+            text = "Gender saved. Does {} have other relations?".format(personName),
+            reply_markup = keyboard
+            )
+        
+        return 'callbacks'
+
+    def newPerson (update, context):
+        Message = update.message
+        personName = Message.text
+
+        #Get userdata
+        data = context.bot_data[Message.from_user.id]
+        
+        #Add new person to exists
+        data[personName] = {"relations": [], 'gender': None, 'selected':True}
+        context.bot_data[Message.from_user.id] = data
+
+        #Save datafile
+        context.dispatcher.persistence.flush()
+
+        #Get gender keyboard from getGenders
+        keyboard = getGenders()
+
+        #Give the user a choice of gender
+        Message.reply_text(
+            text = 'Select {} gender:'.format(personName),
+            reply_markup = keyboard
+            )
+        
+        return 'callbacks'
+        
+        
+    def manageCallbacks (update, context):
+        Query = update.callback_query
+
+        options = {"yes": True, 'no': False}
+
+        if Query.data.startswith('setgender'):
+            return ConversationFirstMenu.changePersonGender(update, context)
+        elif Query.data.startswith('haveOtherRelations'):
+            haveOtherRelations = options[Query.data.split()[1]]
+            if haveOtherRelations:
+                
+                #Get relations keyboard from the getRelations
+                keyboard = getRelations()
+                
+                Query.message.edit_text(
+                    text = "Select relation:",
+                    reply_markup = keyboard
+                    )
+                return 'callbacks'
+            else:
+                keyboard = InlineKeyboardMarkup(
+                    [
+                        [
+                            InlineKeyboardButton(
+                                text = 'Cancel!',
+                                callback_data = 'cancel'
+                                ),
+                            InlineKeyboardButton(
+                                text = 'Save!',
+                                callback_data = 'save'
+                                )
+                            ],
+                        [
+                            InlineKeyboardButton(
+                                text = 'New Person!',
+                                callback_data = 'newPerson'
+                                )
+                            ]
+                        ]
+                    )
+                Query.message.edit_text(
+                    text = 'What do you want to do:',
+                    reply_markup = keyboard
+                    )
+                return 'callbacks'
+        elif Query.data == 'cancel':
+
+            #Delete user data
+            context.bot_data[Query.from_user.id] = {}
+
+            #Save changes
+            context.dispatcher.persistence.flush()
+
+            Query.message.edit_text(
+                text = 'Cancelled!'
+                )
+            
+            #Return quit code from ConversationHandler
+            return ConversationHandler.END
+
+        elif Query.data == 'save':
+
+            #Save data
+            context.dispatcher.persistence.flush()
+
+            #Retrieve userdata
+            data = context.bot_data[Query.from_user.id]
+
+            """
+            Here must be code to write .csv file!
+            """
+
+            Query.message.edit_text(
+                text = 'Saved!'
+                )
+
+            #Return quit code from ConversationHandler
+            return ConversationHandler.END
+
+        elif Query.data == 'newPerson':
+
+            data = context.bot_data[Query.from_user.id]
+
+            for personName, personDetails in data.items():
+                personDetails['selected'] = False
+
+            context.bot_data[Query.from_user.id] = data
+
+            context.dispatcher.persistence.flush()
+
+            Query.message.edit_text(
+                text = 'Okay, enter the person name.'
+                )
+
+            return 'newPerson'
+        elif Query.data.startswith('addPersonRelation'):
+            return ConversationFirstMenu.changePersonRelations(update, context)
+
+def ConvCancel (update, context):
+    context.bot_data[update.message.from_user.id] = {}
+    
+    update.message.reply_text(
+        text = 'Cancelled!'
+        )
+    
+    return ConversationHandler.END
+        
+        
+        
+
 
 def main():
      # Create the Updater and pass it your bot's token.
     # Make sure to set use_context=True to use the new context based callbacks
     # Post version 12 this will no longer be necessary
-    updater = Updater('1414806782:AAEM9Pkr5-Tk4vfFNhKtSffNyeeffhgK0X8',use_context=True)
+    persistence = PicklePersistence('DATA')
+    
+    updater = Updater(
+        token = '1347685746:AAEldd4OUX-JOofIrg9N7bah6ly1yEzTCZ8',
+        use_context = True,
+        persistence = persistence
+        )
     
     # Get the dispatcher to register handlers
     dp = updater.dispatcher
-    ############################# Handlers #########################################
+    ###################################################### Handlers #############################################################
 
 
-    # Register the commands...
+    # ________________________________________________ CommandHandlers _________________________________________________________
+    
     dp.add_handler(CommandHandler('start',start))
-    dp.add_handler(CallbackQueryHandler(menu_actions))
 
-    dp.add_handler(CallbackQueryHandler(first_menu, pattern='m1'))
-    dp.add_handler(CallbackQueryHandler(second_menu, pattern='m2'))
-    dp.add_handler(CallbackQueryHandler(first_submenu,
-                                                    pattern='m1_1'))
-    dp.add_handler(CallbackQueryHandler(second_submenu,
-                                                    pattern='m2_1'))
+    # _______________________________________________ ConversationHandlers _______________________________________________________
+
+    dp.add_handler(
+        ConversationHandler(
+            entry_points = [
+                CallbackQueryHandler(
+                    callback = ConversationFirstMenu.entry,
+                    pattern = 'm1'
+                    )
+                ],
+            states = {
+                'getEA': [
+                    MessageHandler(
+                        filters = Filters.text & ~Filters.command,
+                        callback = ConversationFirstMenu.setEarliestAncestor
+                        )
+                    ],
+                'callbacks': [
+                    CallbackQueryHandler(
+                        callback = ConversationFirstMenu.manageCallbacks
+                        )
+                    ],
+                'newPerson': [
+                    MessageHandler(
+                        filters = Filters.text & ~Filters.command,
+                        callback = ConversationFirstMenu.newPerson
+                        )
+                    ],
+                'setRelation2ndPerson': [
+                    MessageHandler(
+                        filters = Filters.text & ~Filters.command,
+                        callback = ConversationFirstMenu.setRelationFor
+                        )
+                    ]
+                },
+            fallbacks = [
+                CommandHandler(
+                    command = 'cancel',
+                    callback = ConvCancel
+                    )
+                ]
+            )
+        )
 
 
+    # _______________________________________________ CallbackQueryHandlers ______________________________________________________
+    
+    dp.add_handler(CallbackQueryHandler(manageCallbacks))
 
 
-    11
-    # ...and the error handler    
+    # ________________________________________________ ErrorHandler _______________________________________________________________
+    
     dp.add_error_handler(error_handler)
+
+
+    
     updater.start_polling()
     updater.idle()
 if __name__ == '__main__':
